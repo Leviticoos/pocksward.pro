19c64311e9b185d814316fd2453c2db90a7fafaf
Leviticoos <105832125+Leviticoos@users.noreply.github.com>
2023-10-27 14:57:40 -0700

    Homepage
    Imported from my website

---
+++ index.php
@@ -0,0 +1,85 @@
+<html>
+<!page has been copy edited oct 2022>
+<head>
+<link href="css/styles.css" rel="stylesheet" type="text/css">
+<title>
+Leviticus' Website
+</title>
+</head>
+
+<body>
+
+<h1>
+Leviticus' Website
+</h1>
+
+<img src="photos/me.jpg" alt="Who is this handsome fella? I'd love to give him a job or something!" style="display: block; margin-left: auto; margin-right: auto;">
+
+<address><a href = "mailto: levi@leviticusrhoden.com">Send Me an E-Mail!</a></address>
+
+<br>
+<p>
+    My name is Leviticus Rhoden, and I'm a mechanical engineer and tinkerer. I've spent my life solving problems and building things, both as a hobby and as a profession. I am currently an undergrad at the University of Washington, where I do research with the Applied Mathematics department on <a href="/pwhresearch/pwhresearch.php">pilot wave hydrodynamics</a>. I also spend much of my free time making, tinkering, and crafting. 
+    </p><p>
+        If I had a dime for everytime I was working on a project, and found exactly what I was looking for on some html site from the early 2000's, I'd be able to afford my college tuition. To that end, I figured it was time to give back to that comunity. I hope to catalouge and recount some of my projects and the sources that helped make 'em tick.
+</p>
+
+<h2>
+    Table of Contents
+</h2>
+<dt><a href="crafting/crafting.php">Crafting</a></dt>
+    <dd>-<a href="crafting/nonPlanar3D.php"> COMING SOON Non Planar 3D Printing</a></dd>
+    <dd>-<a href="crafting/3d_minis.php"> 3D Printing D&D Minis</a></dd>
+    <dd>-<a href="crafting/recordplayer.php"> Record Player Battery</a></dd>
+    <dd>-<a href="crafting/recordplayer2.php"> Record Player Platter</a></dd><br>
+<dt><a href="cameras/cameras.php">Cameras and Optics</a></dt>
+    <dd>-<a href='cameras/developing.php'>How I Develop B&W Film</a></dd>
+    <dd>-<a href="cameras/digichrome.php"> WIP Digichrome</a></dd>
+    <dd>-<a href="cameras/OM-1 Camera.php"> COMING SOON OM-1 Camera</a></dd>
+    <dd>-<a href="cameras/pony_135_tuneup.php"> Pony 135 Tuneup</a></dd>
+    <dd>-<a href="cameras/thinlenssim.php"> Python Thin Lens Simulator</a></dd>
+    <br>
+<dt><a href="mathinst/mathinst.php">Mathematical Instruments</a></dt>
+    <dd>-<a href="mathinst/straightedge.php"> On the Construction of the Straightedge</a></dd><br>
+<dt>Notes on Projects</a></dt>
+    <dd>-<a href="notes/triodeNotes.php"> Notes on Vacuum Triode Construction </a></dd>
+    <dd>-<a href="notes/up!miniNotes.php"> Notes on Converting an UP! Mini to use Cura </a></dd>
+<dt><a href="pwhresearch/pwhresearch.php">Pilot Wave Hydrodynamics Research</a></dt>
+    <dd>-<a href="pwhresearch/gifmuseum.php"> GIF Museum</a></dd>
+    <dd>-<a href="pwhresearch/shaker.php"> Oil Bath Shaker</a></dd>
+    <dd>-<a href="pwhresearch/whatispwh.php"> What is Pilot Wave Hydrodynamics?</a></dd><br>
+<dt><a href="website/website.php">Website</a></dt>
+    <dd>-<a href="website/example.php"> Example Page</a></dd>
+    <dd>-<a href="website/hostingpi.php"> Hosting a Website on a Raspberry Pi</a></dd>
+    <dd>-<a href="website/othersites.php"> Links</a></dd>
+    <dd>-<a href="website/styleguide.php"> Leviticus' Website Styleguide</a></dd><br>
+    
+<br><br>
+<h2>
+    Chronological Order
+</h2>
+<dt><a href="cameras/developing.php">How I Develop B&W Film</a></dt>
+    <dd>- Dec 2022</dd>
+<dt><a href="cameras/thinlenssim.php">Thin Lens Simulator</a></dt>
+    <dd>- Dec 2022</dd>
+<dt><a href='crafting/3d_minis.php'>3D Printing D&D Minis</a></dt>
+    <dd>- Sept 2022</dd>
+<dt><a href="crafting/recordplayer2.php">Record Player Platter</a></dt>
+    <dd>- July 24th, 2022</dd>
+<dt><a href="cameras/pony_135_tuneup.php">Pony 135 Tuneup</a></dt>
+    <dd>- October 7th, 2021</dd>
+<dt><a href="mathinst/straightedge.php">Straightedge</a></dt>
+    <dd>- September 27th, 2021</dd>
+<dt><a href="crafting/recordplayer.php">Record Player Battery</a></dt>
+    <dd>- August 29th, 2021</dd>
+<dt><a href="pwhresearch/gifmuseum.php">GIF Museum</a></dt>
+    <dd>- June 2021</dd>
+<dt><a href="website/hostingpi.php">Hosting a Website on a Raspberry Pi</a></dt>
+    <dd>- May 2021</dd>
+
+
+<address><a href = "rss.xml"> RSS Feed</a></address>
+<address><a href = "mailto: levi@leviticusrhoden.com">Send Me an E-Mail!</a></address>
+</body>
+
+</html>
